c659648fcc37aa3f4e1b0bb4ba08cb672d99b57c
thanhd <danhthanh418@gmail.com>
2024-05-10 09:11:23 -0700

    update: update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import stylelint from 'vite-plugin-stylelint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    stylelint({
-      formatter: 'compact'
-    })
-  ],
+  plugins: [react()],
   server: {
     port: 3000
   },
