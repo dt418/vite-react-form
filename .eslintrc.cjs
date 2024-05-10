4ddfb2e973950ef82955fc52d5bb6c520ee217fb
thanhd <danhthanh418@gmail.com>
2024-05-10 09:04:47 -0700

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,62 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  settings: {
+    react: {
+      createClass: 'createReactClass',
+      pragma: 'React',
+      fragment: 'Fragment',
+      version: 'detect',
+      flowVersion: '0.53'
+    }
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'eslint-config-prettier',
+    'prettier'
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'prettier', 'react-hooks'],
+  rules: {
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'prettier/prettier': [
+      'warn',
+      {
+        arrowParens: 'always',
+        semi: false,
+        trailingComma: 'none',
+        tabWidth: 2,
+        endOfLine: 'auto',
+        useTabs: false,
+        singleQuote: true,
+        printWidth: 120,
+        jsxSingleQuote: true
+      }
+    ],
+    'react/jsx-filename-extension': 0,
+    'react/react-in-jsx-scope': 0,
+    'react/prop-types': 0,
+    'react/jsx-props-no-spreading': 0,
+
+    'jsx-a11y/click-events-have-key-events': 0,
+    'jsx-a11y/no-static-element-interactions': 0,
+    'jsx-a11y/no-noninteractive-element-interactions': 0,
+
+    'no-use-before-define': 0,
+    'no-unused-vars': 0,
+    'implicit-arrow-linebreak': 0,
+    'consistent-return': 0,
+    'arrow-parens': 0,
+    'object-curly-newline': 0,
+    'operator-linebreak': 0,
+    'import/no-extraneous-dependencies': 0,
+    'import/extensions': 0,
+    'import/no-unresolved': 0,
+    'import/prefer-default-export': 0
+  }
+}
